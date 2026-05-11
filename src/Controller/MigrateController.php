1fbd1bf98780fe19599a397654612518802d6189
hugomoulin2000-design <hugomoulin2000@gmail.com>
2026-05-12 01:21:04 +0200

    Update MigrateController.php

--- src/Controller/MigrateController.php
+++ src/Controller/MigrateController.php
@@ -18,37 +18,27 @@ class MigrateController extends AbstractController
     }
 
    #[Route('/debug-db-info')]
-public function debugDbInfo(\Doctrine\DBAL\Connection $conn): Response
-{
-    $params = $conn->getParams();
+    public function debugDbInfo(Connection $conn): Response
+    {
+        $params = $conn->getParams();
 
-    $info = [
-        'driver' => $conn->getDriver()->getName(),
-        'dbname' => $params['dbname'] ?? null,
-        'host' => $params['host'] ?? null,
-        'port' => $params['port'] ?? null,
-        'user' => $params['user'] ?? null,
-        'schema' => $params['schema'] ?? null,
-        'url' => $params['url'] ?? null,
-    ];
+        return new Response('<pre>' . print_r($params, true) . '</pre>');
+    }
 
-    return new Response('<pre>' . print_r($info, true) . '</pre>');
-}
+    #[Route('/debug-user-query')]
+    public function debugUserQuery(Connection $conn): Response
+    {
+        $sql = "
+            SELECT column_name, data_type 
+            FROM information_schema.columns 
+            WHERE table_schema = 'public'
+            AND table_name = 'user'
+        ";
 
-   #[Route('/debug-user-query')]
-public function debugUserQuery(\Doctrine\DBAL\Connection $conn): Response
-{
-    $sql = "
-        SELECT column_name, data_type 
-        FROM information_schema.columns 
-        WHERE table_schema = 'public'
-        AND table_name = 'user'
-    ";
+        $columns = $conn->fetchAllAssociative($sql);
 
-    $columns = $conn->fetchAllAssociative($sql);
-
-    return new Response('<pre>' . print_r($columns, true) . '</pre>');
-}
+        return new Response('<pre>' . print_r($columns, true) . '</pre>');
+    }
 
 
 
