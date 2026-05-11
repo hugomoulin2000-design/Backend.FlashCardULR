ac104e0c3b05715424a12cdd9dc0f92a22d81622
hugomoulin2000-design <hugomoulin2000@gmail.com>
2026-05-11 22:01:35 +0200

    Update MigrateController.php

--- src/Controller/MigrateController.php
+++ src/Controller/MigrateController.php
@@ -5,18 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Process\Process;
 
 class MigrateController extends AbstractController
 {
     #[Route('/migrate', name: 'app_migrate')]
     public function migrate(): Response
     {
-        $process = new Process(['php', 'bin/console', 'doctrine:migrations:migrate', '--no-interaction']);
-        $process->run();
+        $output = shell_exec('php bin/console doctrine:migrations:migrate --no-interaction 2>&1');
 
-        return new Response(
-            nl2br($process->getOutput() . "\n" . $process->getErrorOutput())
-        );
+        return new Response(nl2br($output));
     }
 }
