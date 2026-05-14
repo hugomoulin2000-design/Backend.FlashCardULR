f638cb283f7a374fb34db4c004eeaacf78ccadef
hugomoulin2000-design <hugomoulin2000@gmail.com>
2026-05-14 16:37:24 +0200

    Update DashboardController.php

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,54 +2,29 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\RevisionLog;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
-#[AdminDashboard(routePath: '/', routeName: 'admin')]
-class DashboardController extends AbstractDashboardController
+class RevisionLogCrudController extends AbstractCrudController
 {
-    public function index(): Response
+    public static function getEntityFqcn(): string
     {
-        return $this->render('admin/dashboard.html.twig');
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // return $this->redirectToRoute('admin_user_index');
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirectToRoute('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return RevisionLog::class;
     }
 
-    public function configureDashboard(): Dashboard
+    public function configureFields(string $pageName): iterable
     {
-        return Dashboard::new()
-            ->setTitle('Projetl2');
-    }
-
-    public function configureMenuItems(): iterable
-    {
-
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-
-        yield MenuItem::section('Gestion des données');
-        yield MenuItem::linkTo(\App\Controller\Admin\DeckCrudController::class, 'Decks', 'fa fa-layer-group');
-        yield MenuItem::linkTo(\App\Controller\Admin\FlashcardCrudController::class, 'Flashcards', 'fa fa-clone');
-        yield MenuItem::linkTo(\App\Controller\Admin\TagCrudController::class, 'Tags', 'fa fa-tags');
-        yield MenuItem::linkTo(\App\Controller\Admin\UserCrudController::class, 'Utilisateurs', 'fa fa-user');
-        yield MenuItem::linkTo(\App\Controller\Admin\RevisionLogCrudController::class, 'Revisions', 'fa fa-layer-group');
-
-        yield MenuItem::section('Compte');
-        yield MenuItem::linkToLogout('Déconnexion', 'fa fa-sign-out');
+        return [
+            IdField::new('id')->hideOnForm(),
+            AssociationField::new('user', 'Utilisateur'),
+            AssociationField::new('deck', 'Deck'),
+            IntegerField::new('flashcards_count', 'Cartes révisées'),
+            IntegerField::new('duration_seconds', 'Durée (secondes)'),
+            DateTimeField::new('created_at', 'Date')->hideOnForm(),
+        ];
     }
 }
